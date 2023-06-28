bad3162d47002d33eead80a263ca6e9657c24aa2
XiangYu Ye <yexiangyu@gmail.com>
2023-06-28 14:16:14 +0800

    linux loongarch64 1.15.1

--- onnxruntime-sys/src/generated/bindings.rs
+++ onnxruntime-sys/src/generated/bindings.rs
@@ -1,3 +1,9 @@
+#[cfg(all(target_os = "linux", target_arch = "loongarch64"))]
+include!(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/src/generated/linux/loongarch64/bindings.rs"
+));
+
 #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
 include!(concat!(
     env!("CARGO_MANIFEST_DIR"),
